15bd911ecda37fb32ebb7eba6da64d73eaea3341
Boris Schrijver <bschrijver@schubergphilis.com>
2017-08-07 09:36:47 +0200

    VPN connections will be setup by Cosmic

--- scripts/strongswan.sh
+++ scripts/strongswan.sh
@@ -6,6 +6,5 @@ mkdir -p /etc/ipsec.d
 cp /tmp/strongswan-starter.ipsec.init /etc/init.d/ipsec
 cp /tmp/ipsec.conf /etc/
 cp /tmp/ipsec.secrets /etc/
-cp /tmp/l2tp.conf /etc/ipsec.d/
 
 chmod +x /etc/init.d/ipsec
