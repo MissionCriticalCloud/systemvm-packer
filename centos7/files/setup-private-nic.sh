004ca7d1933278806005222987746751d8e645df
Boris Schrijver <bschrijver@schubergphilis.com>
2017-09-17 01:51:28 +0200

    Setup private nic

--- centos7/files/setup-private-nic.sh
+++ centos7/files/setup-private-nic.sh
@@ -3,4 +3,77 @@
 set -x
 
 # Start the magic!
-echo "Cosmic has started, yeah!"
+echo "Configuring the private nic!"
+
+CMDLINE="/var/cache/cloud/cmdline"
+CMDLINE_DONE="/var/cache/cloud/cmdline_incoming"
+
+# Wait for the config to arrive
+while [ ! -f ${CMDLINE_DONE} ]
+do
+  sleep 1
+done
+cat ${CMDLINE}
+
+DOMAIN=
+DNS1=
+DNS2=
+NEW_HOSTNAME=
+PRIVATE_NIC_IPV4=
+PRIVATE_NIC_IPv4_NETMASK=
+PRIVATE_NIC_MAC=
+
+for i in $(cat ${CMDLINE})
+  do
+    KEY=$(echo ${i} | cut -d= -f1)
+    VALUE=$(echo ${i} | cut -d= -f2)
+    case ${KEY} in
+      domain)
+        DOMAIN=${VALUE}
+        ;;
+      dns1)
+        DNS1=${VALUE}
+        ;;
+      dns2)
+        DNS2=${VALUE}
+        ;;
+      name)
+        NEW_HOSTNAME=${VALUE}
+        ;;
+      eth0ip)
+        PRIVATE_NIC_IPV4=${VALUE}
+        ;;
+      eth0mask)
+        PRIVATE_NIC_IPv4_NETMASK=${VALUE}
+        ;;
+      eth0mac)
+        PRIVATE_NIC_MAC=${VALUE}
+        ;;
+      *)
+        ;;
+    esac
+done
+
+# Setup private nic
+cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
+DEVICE="eth0"
+IPV6INIT="no"
+BOOTPROTO="none"
+ONBOOT="yes"
+HWADDR="${PRIVATE_NIC_MAC}"
+IPADDR="${PRIVATE_NIC_IPV4}"
+NETMASK="${PRIVATE_NIC_IPv4_NETMASK}"
+EOF
+
+# Setup hostname
+hostnamectl set-hostname ${NEW_HOSTNAME}
+
+# Setup dns
+cat > /etc/resolv.conf << EOF
+search ${DOMAIN}
+`if [ ! -z "${DNS1}" ]; then echo "nameserver ${DNS1}"; fi`
+`if [ ! -z "${DNS2}" ]; then echo "nameserver ${DNS2}"; fi`
+EOF
+
+# Reload the private nic
+ifdown eth0; ifup eth0
