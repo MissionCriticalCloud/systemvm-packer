99b2ce289032ce6b9e3b07e9182a120692c3f1b3
Boris Schrijver <bschrijver@schubergphilis.com>
2017-09-17 18:10:49 +0200

    tmp

--- centos7/scripts/conntrack.sh
+++ centos7/scripts/conntrack.sh
@@ -2,12 +2,9 @@
 
 set -x
 
-date
+grep nf_conntrack_ipv4 /etc/modules-load.d/10-conntrack-modules.conf && return
 
-load_conntrack_modules () {
-  grep nf_conntrack_ipv4 /etc/modules-load.d/10-conntrack-modules.conf && return
-
-  cat >> /etc/modules-load.d/10-conntrack-modules.conf << EOF
+cat >> /etc/modules-load.d/10-conntrack-modules.conf << EOF
 nf_conntrack_ipv4
 nf_conntrack
 nf_conntrack_ftp
@@ -16,6 +13,3 @@ nf_conntrack_proto_gre
 nf_nat_tftp
 nf_nat_ftp
 EOF
-}
-
-return 2>/dev/null || load_conntrack_modules
